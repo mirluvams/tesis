07f4b76829d297b053e01f7d8fb36ecb23fb2258
Miriam Valdez <mirluvams@gmail.com>
2024-10-02 21:12:49 -0600

    results

--- 5-model-train.py
+++ 5-model-train.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import sklearn.metrics
 import torchvision.transforms.v2 as transforms
-from transformers import AutoImageProcessor, AutoModel, TrainingArguments, Trainer, PreTrainedModel, PretrainedConfig, AutoConfig
+from transformers import AutoImageProcessor, AutoModelForImageClassification, TrainingArguments, Trainer, PreTrainedModel, PretrainedConfig, AutoConfig
 from datasets import Dataset, load_from_disk
 from evaluate import load as load_metric
 from PIL import Image, ImageFile
@@ -16,53 +16,20 @@ import os
 model_names=[
     "microsoft/swinv2-tiny-patch4-window16-256",
     "microsoft/swinv2-base-patch4-window16-256",
-    "facebook/dinov2-base",
     "microsoft/beit-base-patch16-224",
     "google/vit-base-patch16-224",
 ]
 
-image_size=256
-model_base=model_names[0]
-model_output_name="swinv2-tiny"
+image_size=224
+model_base=model_names[3]
+model_output_name="vit-base"
+model_output=f"./models/{model_output_name}"
+model_output_tmp=f"~/tmp/models/{model_output_name}"
 
-class ImageMultiRegressionConfig(PretrainedConfig):
-    def __init__(
-        self,
-        output_size=3,
-        init_checkpoint=None,
-        **kwargs,
-    ):
-        self.output_size=output_size
-        self.init_checkpoint=init_checkpoint
-        super().__init__(**kwargs)
-
-
-class ImageMultiRegressionModel(PreTrainedModel):
-    config_class=ImageMultiRegressionConfig
-    def __init__(self, config, loss=nn.MSELoss()):
-        super().__init__(config)
-        
-        self.inner_model = AutoModel.from_pretrained(config.init_checkpoint)
-        self.classifier = nn.Linear(self.inner_model.config.hidden_size, config.output_size)
-        self.loss=loss
-    
-    def forward(self, pixel_values, labels=None):
-        outputs = self.inner_model(pixel_values=pixel_values)
-        cls_output = outputs.last_hidden_state[:, 0, :]  # image embedding
-        values = self.classifier(cls_output)
-        loss = None
-        if labels is not None:
-            loss = self.loss(values.view(-1), labels.view(-1))
-        return (loss, values) if loss is not None else values
-
-
-#image_processor = AutoImageProcessor.from_pretrained(model_name)
 try:
-    model=ImageMultiRegressionModel.from_pretrained(model_output)
+    model=AutoModelForImageClassification.from_pretrained(model_output)
 except:
-    print("INITIALIZING NEW MODEL AS PRETRAINED LOAD FAILED")
-    config=ImageMultiRegressionConfig(output_size=3, init_checkpoint=model_base)
-    model=ImageMultiRegressionModel(config)
+    model=AutoModelForImageClassification.from_pretrained(model_base)
 
 
 #model_name="microsoft/swinv2-base-patch4-window16-256"
@@ -116,24 +83,23 @@ trainer_args = TrainingArguments(
     num_train_epochs=25,
     load_best_model_at_end=True,
     remove_unused_columns=False,
-    metric_for_best_model="R2_test",
+    metric_for_best_model="f1",
     greater_is_better=True,
     push_to_hub=False,
     report_to=[]
 )
 
 
-_columns=["light_level","fume_strength","explosion_strength"]
 def collate_fn(examples):
     pixel_values = torch.stack([example["pixel_values"] for example in examples])#.to(DEVICE)
-    labels =  torch.stack([torch.tensor([example[c] for c in _columns]) for example in examples])#.to(DEVICE)
+    labels = torch.tensor([example["class"] for example in examples])
     return {"pixel_values": pixel_values, "labels": labels, }
 
 def compute_metrics(pred):
-    x,y=pred.predictions, pred.label_ids
-    return {"MSE":sklearn.metrics.mean_squared_error(x, y),
-           "MAE":sklearn.metrics.mean_absolute_error(x,y),
-           "R2_test":sklearn.metrics.r2_score(x,y)}
+    x,y=np.argmax(pred.predictions, axis=1), pred.label_ids
+    #print(x,y)
+    return {"accuracy":sklearn.metrics.accuracy_score(x, y),
+           "f1":sklearn.metrics.f1_score(x,y, average='weighted')}
 
 trainer = Trainer(
     model,
@@ -144,9 +110,9 @@ trainer = Trainer(
     compute_metrics=compute_metrics,
     data_collator=collate_fn
 )
-
+print(trainer.evaluate())
 print(trainer.train())
 
 #trainer.save_metrics("all", )
-trainer.save_model(f"models/{model_output_name}")
+trainer.save_model(model_output)
 trainer.save_state()
